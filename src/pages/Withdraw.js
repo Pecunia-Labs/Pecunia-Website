fe5fbf9dd2233c16729da0b897ea1d78f8821737
ysongit <87449884+ysongit@users.noreply.github.com>
2022-09-16 16:05:02 -0400

    remove input for NFT ID and use Moralis API instead

--- src/pages/Withdraw.js
+++ src/pages/Withdraw.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Container, FormControl, FormLabel, Box, GridItem, Spinner, ButtonGroup, Input, Avatar, Image, Heading, Button, Text } from '@chakra-ui/react';
+import { Container, FormControl, FormLabel, Box, GridItem, Spinner, ButtonGroup, Input, Select, Heading, Button } from '@chakra-ui/react';
 import { ethers } from 'ethers';
 
 import { NFTCONTRACT_ADDRESS, CONTRACT_ADDRESS }  from '../contractdata/config';
 
-function Withdraw({ contractHeir, contractNFT }) {
+function Withdraw({ ethaddress, contractHeir, contractNFT }) {
   const changePage = useNavigate();
 
   const [password, setPassword] = useState("");
   const [amount, setAmount] = useState("");
   const [owner, setOwner] = useState("");
   const [nftid, setnftid] = useState("");
+  const [nfts, setNFTs] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const options = {method: 'GET', headers: {accept: 'application/json', 'X-API-Key': 'test'}};
+
+    fetch(`https://deep-index.moralis.io/api/v2/${ethaddress}/nft?chain=mumbai&format=decimal&token_addresses=${NFTCONTRACT_ADDRESS}`, options)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        setNFTs(response.result);
+      })
+      .catch(err => console.error(err));
+  }, [])
 
   const withdrawSignature = async () => {
     try{
@@ -52,7 +65,7 @@ function Withdraw({ contractHeir, contractNFT }) {
   }
   return (
     <GridItem colSpan={5}>
-      <center maxW='1100px' mt="3">
+      <center maxw='1100px' mt="3">
         <Box borderWidth='1px' borderRadius='lg'  borderColor="teal" overflow='hidden' p="5" width="500px" mt="32">
           <Heading fontSize='2xl' mb="3">Withdraw</Heading>
           <FormControl mb="3">
@@ -69,7 +82,11 @@ function Withdraw({ contractHeir, contractNFT }) {
           </FormControl>
           <FormControl mb="3">
             <FormLabel>NFT ID</FormLabel>
-            <Input onChange={(e) => setnftid(e.target.value)} />
+            <Select placeholder='Select Heir NFT' onChange={(e) => setnftid(e.target.value)}>
+              {nfts.map(n => (
+                <option key={n.token_id} value={n.token_id}>{n.token_id}</option>
+              ))}
+            </Select>
           </FormControl>
          
           {loading
